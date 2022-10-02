ba7c43d9565be1af7ae09836303163566b32ea8b
Kelly Gravelyn <kelly@kellyrenee.me>
2022-10-01 22:23:49 -0700

    Just link YouTube videos like tweets to get standard embedding, like tweets

--- youtube.rb
+++ youtube.rb
@@ -26,45 +26,14 @@ def get_videos
 		# Skip OWL costreams
 		next nil if title.downcase.include?("costream")
 
-		id = entry.css("yt|videoId").inner_text
-		timestamp = entry.css("published").inner_text
-		description = entry.css("media|description").inner_text
-		image = entry.css("media|thumbnail").attribute("url").value
-
-		# Helpfully Overwatch uses hyphens and underscores to separate the actual
-		# description from a bunch of link spam so we can start there.
-		end_description = description.index("\n-\n") || description.index("\n_\n")
-		description = description[0...end_description].strip
-
-		# We can also look for a line of hash tags and strip that off.
-		description = description.lines.select do |l|
-			next true if l == "\n"
-			!l.split(" ").all? { |w| w[0] == "#" }
-		end.join().strip
-
-		{
-			"title" => title,
-			"description" => description,
-			"url" => "https://www.youtube.com/watch?v=#{id}",
-			"image" => image,
-			"timestamp" => Time.parse(timestamp).iso8601
-		}
+		entry.css("yt|videoId").inner_text
 	end.compact
 end
 
-def post_to_discord(items)
-	items.each_slice(10).reverse_each do |slice|
+def post_to_discord(ids)
+	ids.reverse_each do |id|
 		data = {
-			"embeds" => slice.reverse.map do |item|
-				{
-					"title" => item["title"],
-					"color" => 15823666,
-					"description" => item["description"],
-					"url" => item["url"],
-					"image" => {"url" => item["image"]},
-					"timestamp" => item["timestamp"]
-				}
-			end
+			"content": "https://youtube.com/watch?v=#{id}"
 		}
 
 		if ENV["NO_DISCORD"] != nil && ENV["NO_DISCORD"] != ""
@@ -84,6 +53,8 @@ def post_to_discord(items)
 		if response.status != 204
 			puts "Failed to post to Discord: (#{response.status}) #{response.body}"
 		end
+
+		sleep(1)
 	end
 end
 
