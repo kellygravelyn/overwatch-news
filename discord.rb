da67b171d02bef44a4c61877f30a0accb433b9fb
Kelly Gravelyn <kelly@kellyrenee.me>
2022-10-13 21:26:58 -0700

    Moved YouTube over to shared helpers

--- discord.rb
+++ discord.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "bundler/setup"
 require "dotenv/load"
 require "excon"
+require "json"
 
 module Discord
 	def self.post(data)
--- youtube.rb
+++ youtube.rb
@@ -3,13 +3,10 @@ require "bundler/setup"
 require "dotenv/load"
 require "excon"
 require "nokogiri"
-require "json"
-require "time"
-require "date"
+require_relative "cache"
+require_relative "discord"
 
-CACHE_FILE_PATH = "youtube.json"
-
-def get_videos
+def get_video_ids
 	response = Excon.get(
 		"https://www.youtube.com/feeds/videos.xml?channel_id=UClOf1XXinvZsy4wKPAkro2A",
 	)
@@ -32,43 +29,23 @@ end
 
 def post_to_discord(ids)
 	ids.reverse_each do |id|
-		data = {
+		Discord.post({
 			"content" => "https://youtube.com/watch?v=#{id}"
-		}
-
-		if ENV["NO_DISCORD"] != nil && ENV["NO_DISCORD"] != ""
-			puts "WOULD POST TO DISCORD:"
-			puts JSON.pretty_generate(data)
-			next
-		end
-
-		response = Excon.post(
-			ENV["DISCORD_HOOK_URL"],
-			body: data.to_json,
-			headers: {
-				"Content-Type" => "application/json"
-			}
-		)
-
-		if response.status != 204
-			puts "Failed to post to Discord: (#{response.status}) #{response.body}"
-		end
-
+		})
 		sleep(1)
 	end
 end
 
-latest = []
-if File.exists?(CACHE_FILE_PATH)
-	latest = JSON.parse(File.read(CACHE_FILE_PATH))
+cache = Cache.new("youtube")
+
+cached_ids = cache.read
+
+ids = get_video_ids
+
+new_ids = ids - cached_ids
+
+if new_ids.size > 0
+	post_to_discord(new_ids)
 end
 
-items = get_videos
-
-new_items = items - latest
-
-if new_items.size > 0
-	post_to_discord(new_items)
-end
-
-File.write(CACHE_FILE_PATH, JSON.pretty_generate(items))
+cache.write(ids)
